6590455186a6b90304f098bf09175373ed37592d
SAKTHIVEL A <Sakthiwebnet@gmail.com>
2025-08-10 08:50:21 +0530

    Add files via upload

---
+++ Designpage/Usingdock/FORLOGIN.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Usingdock
+{
+    /// <summary>
+    /// Interaction logic for FORLOGIN.xaml
+    /// </summary>
+    public partial class FORLOGIN : Window
+    {
+        public FORLOGIN()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            txtlogin.Text=txtlogin.Text+"@gmail.com";
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            txtlogin.Text = string.Empty;
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtlogin.Text) )
+            {
+                MessageBox.Show("Please enter username");
+            }
+            else
+            {
+                MessageBox.Show("Login successful!");
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Designpage/Usingdock/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using Usingdock.Insidepage;
+namespace Usingdock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Newing_Click(object sender, RoutedEventArgs e)
+        {
+            Addpage addpage = new Addpage();
+            Mainwindow.Content = addpage;
+
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            Pagedock pagedock = new Pagedock();
+            Mainwindow.Content = pagedock;
+
+        }
+
+        private void saving_Click(object sender, RoutedEventArgs e)
+        {
+            Pagepass  pagepass = new Pagepass();
+            Mainwindow.Content = pagepass;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if(Mainwindow.CanGoBack)
+            {
+                Mainwindow.GoBack();
+            }
+           
+        }
+
+        private void FORWARD_Click(object sender, RoutedEventArgs e)
+        {
+            if (Mainwindow.CanGoForward)
+            {
+                Mainwindow.GoForward();
+            }
+           
+
+        }
+    }
+}
